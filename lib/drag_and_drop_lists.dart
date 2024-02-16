42687cfd71e7b844182a390dd43d6377c436f35d
BenGicheru <bensgich@gmail.com>
2024-02-16 11:27:58 +0300

    Auto scroll updates

--- lib/drag_and_drop_lists.dart
+++ lib/drag_and_drop_lists.dart
@@ -762,19 +762,21 @@ class DragAndDropListsState extends State<DragAndDropLists> {
       print('pixels=${scrollController.position.pixels}');
       print('minScrollExtent=${scrollController.position.minScrollExtent}');
       print('maxScrollExtent=${scrollController.position.maxScrollExtent}');
+      double minScrollExtent = top;
+      double maxScrollExtent = bottom;
       if (pointerYPosition < (top + _scrollAreaSize) &&
           scrollController.position.pixels >
-              scrollController.position.minScrollExtent) {
+              minScrollExtent) {
         final overDrag =
             max((top + _scrollAreaSize) - pointerYPosition, _overDragMax);
-        newOffset = max(scrollController.position.minScrollExtent,
+        newOffset = max(minScrollExtent,
             scrollController.position.pixels - overDrag / _overDragCoefficient);
       } else if (pointerYPosition > (bottom - _scrollAreaSize) &&
           scrollController.position.pixels <
-              scrollController.position.maxScrollExtent) {
+              maxScrollExtent) {
         final overDrag = max<double>(
             pointerYPosition - (bottom - _scrollAreaSize), _overDragMax);
-        newOffset = min(scrollController.position.maxScrollExtent,
+        newOffset = min(maxScrollExtent,
             scrollController.position.pixels + overDrag / _overDragCoefficient);
       }
     }
